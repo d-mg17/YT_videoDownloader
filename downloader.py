799cfb65ffb3f45aa4a12be163e10e9d7bd46a27
Diego Martinez <d.mg17@outlook.com>
2022-07-15 13:15:32 -0400

    Added documentation

--- downloader.py
+++ downloader.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+#import librarys
 from pytube import YouTube
 from sys import argv
 
+#Save link in a variabe. Link was passed as a parameter 
 link = argv[1]
 yt = YouTube(link)
 
@@ -12,6 +14,8 @@ print("Views:", yt.views, "\n")
 
 print("\n")
 
+#getting video with highest res
 video= yt.streams.get_highest_resolution()
 
+#Saving the video on 'Videos' folder
 video.download("C:\\Users\\diego\\Videos")
